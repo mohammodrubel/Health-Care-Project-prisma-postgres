061cd948e01ddcc3e97d63051a3d3cb0d2c76245
rubelrocks <md.rubel007@hotmail.com>
2024-04-15 00:01:45 +0600

    use operator OR AND

--- src/App/modules/Admin/admin_Service.ts
+++ src/App/modules/Admin/admin_Service.ts
@@ -1,31 +1,35 @@
-import { PrismaClient } from "@prisma/client"
+import { Prisma, PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const getAllAdminService = async (params:any)=>{
-    console.log(params)
-    const result = await prisma.admin.findMany({
-        where:{
-            OR:[
-                {
-                    name:{
-                        contains:params?.searchTerm,
-                        mode:'insensitive'
-                    }
-                },
-                {
-                    email:{
-                        contains:params?.email,
-                        mode:'insensitive'
-                    }
-                },
-            ]
-        }
-    })
-    return result
-}
+const getAllAdminService = async (params: any) => {
+  const addCondition:Prisma.AdminWhereInput[] = [];
+  if (params.searchTerm) {
+    addCondition.push({
+      OR: [
+        {
+          name: {
+            contains: params?.searchTerm,
+            mode: "insensitive",
+          },
+        },
+        {
+          email: {
+            contains: params?.email,
+            mode: "insensitive",
+          },
+        },
+      ],
+    });
+  }
+  const whareCondition:Prisma.AdminWhereInput = { AND: addCondition };
 
+  const result = await prisma.admin.findMany({
+    where: whareCondition,
+  });
+  return result;
+};
 
 export const adminService = {
-    getAllAdminService
-}
+  getAllAdminService,
+};
