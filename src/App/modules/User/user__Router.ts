371115302214608d1740e2be3793bcd15f4cfc03
rubelrocks <md.rubel007@hotmail.com>
2024-04-21 10:00:59 +0600

    upload file in my server

--- src/App/modules/User/user__Router.ts
+++ src/App/modules/User/user__Router.ts
@@ -1,7 +1,16 @@
-import express, { Request, Response } from 'express';
-import { userController } from './user__Controller';
-const router = express.Router()
+import express from "express";
+import { userController } from "./user__Controller";
+import Auth from "../../Middleware/Auth";
+import { User__Role } from "@prisma/client";
+import { file__upload } from "../../Middleware/file__upload";
+const router = express.Router();
 
-    router.post('/create-admin',userController.createAdminController)
 
-export const userRouter = router  
+
+router.post(
+  "/create-admin",file__upload.single('file'),
+  Auth(User__Role.ADMIN, User__Role.SUPER_ADMIN),
+  userController.createAdminController
+);
+
+export const userRouter = router;
